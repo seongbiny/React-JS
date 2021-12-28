986d230ee86edccd2b8ed89306de6ad2dcf4e1bc
seongbiny <s30ngb1n@gmail.com>
2021-12-29 07:26:46 +0900

    211229

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ import Subject from "./components/Subject";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "WEB", sub: "World wide Web!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is for information" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JS", desc: "JS is for interactive" },
+      ],
+    };
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <Subject title="React" sub="For UI!"></Subject>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject>
         <TOC></TOC>
         <Content
           title="HTML"
