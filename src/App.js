7a8f5fdd38dc2670b311698ba81301839b9e2c93
seongbiny <s30ngb1n@gmail.com>
2022-01-01 18:54:34 +0900

    220101

--- src/App.js
+++ src/App.js
@@ -30,10 +30,28 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <Subject
+        {/* <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}
-        ></Subject>
+        ></Subject> */}
+        <header>
+          <h1>
+            <a
+              href="/"
+              onClick={function (e) {
+                console.log(e);
+                e.preventDefault();
+                // this.state.mode = "welcome";
+                this.setState({
+                  mode: "welcome",
+                });
+              }.bind(this)}
+            >
+              {this.state.subject.title}
+            </a>
+          </h1>
+          {this.state.subject.sub}
+        </header>
         <TOC></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
