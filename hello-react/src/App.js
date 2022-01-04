93e871015d80aaf2c52563118890578b8e5664dd
yunseongbin <s30ngb1n@gmail.com>
2022-01-04 22:35:23 +0900

    220104

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            안녕하세요 리액트
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
