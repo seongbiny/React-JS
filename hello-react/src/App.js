dbcccc29217bb3e245ef302ee583287e0376e904
yunseongbin <s30ngb1n@gmail.com>
2022-01-05 22:44:17 +0900

    5장 ref:DOM에 이름 달기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,15 @@
 import { Component } from 'react';
-import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
 class App extends Component {
   render() {
     return (
-      <ValidationSample />
+      <div>
+        <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
     )
   }
 }
